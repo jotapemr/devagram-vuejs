5f04592d852838137381de7fc150ab95417cf629
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-03-30 18:43:38 -0300

    part 1

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import router from './router'
 
 const app = createApp(App)
 
+import '../src/assets/styles/global.scss'
+
 app.use(createPinia())
 app.use(router)
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,23 +21,25 @@
     });
 </script>
 <template>
-    <div>
-        <img src="../assets/imagens/logo.svg" alt="Logo Devagram"/>
+    <div class="container-publico">
+        <img src="../assets/imagens/logo.svg" alt="Logo Devagram" class="logo"/>
         <form>
-            <p v-if="erro">{{erro}}</p>
-            <div>
+            <p v-if="erro" class="error">{{erro}}</p>
+            <div class="input">
                 <img src="../assets/imagens/login.svg" alt="Insira o Login"/>
                 <input type="tex" v-model="login"/>    
             </div>
-            <div>
+            <div class="input">
                 <img src="../assets/imagens/senha.svg" alt="Insira a Senha"/>
                 <input type="password" v-model="senha"/>
             </div>
             <button @click.enter.prevent="efetuarLogin">Login</button>
-            <div>
+            <div class="link">
                 <p>Não possui uma conta?</p>
                 <a>Faça seu cadastro agora!</a>
             </div>
         </form>
     </div>
 </template>
+
+<style lang="scss" src="@/assets/styles/paginaPublica.scss"/>
