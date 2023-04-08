1816d9e007d89a1cda5b765fa594a689e8e15255
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-04-08 14:30:56 -0300

    ações do cabeçalho

--- src/components/HeaderPerfil.vue
+++ src/components/HeaderPerfil.vue
@@ -1,10 +1,16 @@
 <script lang="ts">
-    import { defineComponent } from 'vue';
-    import HeaderAcoes from './HeaderAcoes.vue';
-import Avatar from './Avatar.vue';
-
-
-    export default defineComponent({
+import { defineComponent } from 'vue'
+import HeaderAcoes from './HeaderAcoes.vue'
+import Avatar from './Avatar.vue'
+import router from '@/router';
+import {UsuarioServices} from '../services/UsuarioServices';
+const usuarioServices = new UsuarioServices();
+export default defineComponent({
+    setup() {
+        return {
+            loggedId: localStorage.getItem('_id')
+        }
+    },
     props: {
         usuario: {} as any,
         title: String,
@@ -13,44 +19,63 @@ import Avatar from './Avatar.vue';
         showRight: Boolean,
         isRightIcon: Boolean,
     },
-    components: { HeaderAcoes, Avatar }
-})
-
+    components: { HeaderAcoes, Avatar },
+    computed: {
+        obterTextoBotaoPrincipal() {
+            if (this.usuario?._id === this.loggedId) {
+                return "Editar Perfil";
+            } else if (!this.usuario?.segueEsseUsuario) {
+                return "Seguir";
+            }
+            return "Deixar de seguir";
+        }
+    },
+    methods: {
+        async acaoBotao() {
+            if (this.usuario?._id === this.loggedId) {
+                return router.push({ name: 'editar' });
+            }
+            try {
+                await usuarioServices.togglFollow(this.usuario?._id);
+                this.usuario.segueEsseUsuario = !this.usuario.segueEsseUsuario;
+                if(this.usuario.segueEsseUsuario){
+                    this.usuario.seguidores += 1;
+                }else{
+                    this.usuario.seguidores -= 1;
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    }
+});
 </script>
 <template>
     <div class="container-header-perfil">
-        <HeaderAcoes
-            :title="title"
-            :showLeft="showLeft"
-            :isLeftIcon="isLeftIcon"
-            :showRight="showRight"
-            :isRightIcon="isRightIcon"
-        />
+        <HeaderAcoes :title="title" :showLeft="showLeft" :isLeftIcon="isLeftIcon" :showRight="showRight"
+            :isRightIcon="isRightIcon" />
 
         <div class="perfil">
-            <Avatar :imagem="usuario?.avatar"/>
+            <Avatar :imagem="usuario?.avatar" />
             <div class="infos">
                 <div class="dados">
                     <div class="status">
-                        <strong>{{ usuario?.publicacoes }}</strong>
+                        <strong>{{usuario?.publicacoes}}</strong>
                         <span>Publicações</span>
                     </div>
-
                     <div class="status">
-                        <strong>{{ usuario?.seguidores }}</strong>
+                        <strong>{{usuario?.seguidores}}</strong>
                         <span>Seguidores</span>
                     </div>
-
                     <div class="status">
-                        <strong>{{ usuario?.seguindo }}</strong>
+                        <strong>{{usuario?.seguindo}}</strong>
                         <span>Seguindo</span>
                     </div>
                 </div>
-
-                <button>Seguir</button>
+                <button :class="{principal: !usuario?.segueEsseUsuario ?? false}"
+                    @click="acaoBotao">{{obterTextoBotaoPrincipal}}</button>
             </div>
         </div>
     </div>
 </template>
-
-<style lang="scss" src="@/assets/styles/headerPerfil.scss"/>
+<style lang="scss" src="@/assets/styles/headerPerfil.scss" />
--- src/services/UsuarioServices.ts
+++ src/services/UsuarioServices.ts
@@ -11,4 +11,8 @@ export class UsuarioServices extends HttpApiService{
         }
         return await this.get('/usuario')
     }
+
+    async togglFollow(id: String){
+        return await this.put('/seguir?id='+id)
+    }
 }
